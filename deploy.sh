173a45b40383de64459acb0dd73ae34f8363957e
cannin <cannin@gmail.com>
2016-08-13 11:54:16 -0400

    Test

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+rm -rf tmp || exit 0
+mkdir tmp
+
+# config
+git config --global user.name ${GIT_NAME}
+git config --global user.email ${GIT_EMAIL}
+
+# deploy
+cd tmp
+mkdir travis
+
+cp $TRAVIS_BUILD_DIR/Specification/*.pdf travis
+
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
